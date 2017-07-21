da256819ceaf429973435bb6e2d640e42cb6d271
Dilong-D <dmarkowski19@gmail.com>
2017-07-21 20:51:29 +0200

    Konfiguracja bluetooth, diod led, przyciskow, timerow i ich przerwan, PWM na koła, enkoderow

--- LineFollower/Debug/makedep.mk
+++ LineFollower/Debug/makedep.mk
@@ -2,5 +2,9 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
+bluetooth.c
+
+config.c
+
 main.c
 
